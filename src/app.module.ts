2df7ae5b271f26f96986f2a3128433270fab65eb
Waallaacee <112882132+Waallaacee@users.noreply.github.com>
2022-12-05 12:34:12 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,17 +12,7 @@ import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    // type: 'mysql',
-    // host: 'localhost',
-    // port: 3306,
-    // username: 'root',
-    // password: 'root',
-    // database: 'db_blogpessoal',
-    // entities: [Postagem, Tema, Usuario],
-    // synchronize: true
-   //  }),
-     TypeOrmModule.forRoot({
+      TypeOrmModule.forRoot({
        type: 'postgres',
        url: process.env.DATABASE_URL,
        logging: false,
